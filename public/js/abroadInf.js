c30a98c6c74d6856c1799596c08afd03597cfe7e
JunSeong Bae <gentlmanjun@naver.com>
2021-12-11 04:28:27 +0900

    국외 발생 현황 완성, api 이용

---
+++ public/js/abroadInf.js
@@ -0,0 +1,109 @@
+/* 날짜 구하기 */
+
+var today = new Date();
+
+var lastMonth = new Date(today.setDate(today.getDate() - 7)); //7일전 날짜
+
+var lastMonthDate =
+  lastMonth.getFullYear() +
+  "" +
+  ("00" + (lastMonth.getMonth() + 1)).slice(-2) +
+  "" +
+  ("00" + lastMonth.getDate()).slice(-2);
+var todayDate =
+  today.getFullYear() +
+  "" +
+  ("00" + (today.getMonth() + 1)).slice(-2) +
+  "" +
+  ("00" + today.getDate()).slice(-2);
+
+/*xml 데이터 받아오기*/
+
+var xhr = new XMLHttpRequest();
+var url =
+  "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19NatInfStateJson"; /*URL*/
+var queryParams =
+  "?" +
+  encodeURIComponent("serviceKey") +
+  "=" +
+  "%2BoIYTmUUFCPspL03m39Sa5R35t7ygqHkM8mpml78dw14E0OXd68XcaMzBk9PGHSJTabR3KAYDdAww%2FQcn6V2DA%3D%3D"; /*Service Key*/
+queryParams +=
+  "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+queryParams +=
+  "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /**/
+queryParams +=
+  "&" +
+  encodeURIComponent("startCreateDt") +
+  "=" +
+  encodeURIComponent(lastMonthDate); /*1주일 전 날짜*/
+queryParams +=
+  "&" +
+  encodeURIComponent("endCreateDt") +
+  "=" +
+  encodeURIComponent(todayDate); /*오늘 날짜*/
+
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    nodeValfunc(this); // this == xhr
+  }
+};
+
+xhr.open("GET", url + queryParams);
+xhr.send("");
+
+function nodeValfunc(xml) {
+  var xmlDoc = xml.responseXML;
+
+  var areaName = xmlDoc.getElementsByTagName("areaNm");
+  var nationName = xmlDoc.getElementsByTagName("nationNm");
+  var nationDefCnt = xmlDoc.getElementsByTagName("natDefCnt");
+  var nationDeathCnt = xmlDoc.getElementsByTagName("natDeathCnt");
+
+  var arr = Array.from(Array(areaName.length), () => Array(4).fill(null));
+  var name = areaName[0].childNodes[0].nodeValue;
+  var num = 0;
+
+  for (i = 0; i < areaName.length; i++) {
+    if (
+      i != areaName.length - 1 &&
+      name == areaName[i].childNodes[0].nodeValue
+    ) {
+      num++;
+      arr[i][0] = null;
+      arr[i][1] = null;
+    } else {
+      arr[i - 1][0] = num;
+      arr[i - 1][1] = name;
+      name = areaName[i].childNodes[0].nodeValue;
+      num = 1;
+    }
+    arr[i][2] = nationName[i].childNodes[0].nodeValue;
+    arr[i][3] = nationDefCnt[i].childNodes[0].nodeValue;
+    arr[i][4] = nationDeathCnt[i].childNodes[0].nodeValue;
+  }
+
+  var txt = "";
+  for (j = areaName.length - 1; j >= 0; j--) {
+    txt += "<tr>";
+
+    if (arr[j][1]) {
+      txt += "<th rowspan = " + arr[j][0] + ">" + arr[j][1] + "</th>";
+    }
+
+    if (arr[j][2] != "한국") {
+      txt +=
+        "<td>" +
+        arr[j][2] +
+        "</td>" +
+        "<td>" +
+        arr[j][3] +
+        "명 (사망 " +
+        arr[j][4] +
+        ")" +
+        "</td>" +
+        "</tr>";
+    }
+  }
+
+  document.getElementById("data").innerHTML = txt;
+}
