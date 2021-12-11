b353d1daee49b21c8ec42695e34148fb78d1b6d2
JunSeong Bae <gentlmanjun@naver.com>
2021-12-11 23:21:15 +0900

    백신 접종 안내 완성

---
+++ public/js/inoculation.js
@@ -0,0 +1,7 @@
+const kdca = document.querySelector("#kdca");
+
+kdca.addEventListener("click", () => {
+  window.open(
+    "https://www.kdca.go.kr/board/board.es?mid=a20504000000&bid=0014&act=view&list_no=713037&tag=&nPage=1"
+  );
+});
